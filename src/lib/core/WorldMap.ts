faa4b70033b792e4b1f17028f871c19ad6ff2d38
David J. Davydov <davydovjd611@gmail.com>
2021-02-21 05:52:39 -0500

    Fix the loader not loading assets (see description)
    This seems to be an issue with `new Loader()` as the shared loader works
    perfectly fine. This means that either everything will need to use the
    shared loader and that future versions of loadAssets and loadAsset will
    need to check the shared loader for whether the assets are loaded or
    not, or someone will need to do some technical reasearch and figure out
    what's going on. Until then pixi.js will throw an error every time one
    of the methods try to load an existing asset.

--- src/lib/core/WorldMap.ts
+++ src/lib/core/WorldMap.ts
@@ -1,28 +1,28 @@
 import { Container, Loader } from 'pixi.js';
 const resourceArray = [
-    'img/world_map/bg1.png',
-    'img/world_map/bg2.png',
-    'img/world_map/bird1.png',
-    'img/world_map/bird2.png',
-    'img/world_map/cactus1.png',
-    'img/world_map/cactus2.png',
-    'img/world_map/city1.png',
-    'img/world_map/city2.png',
-    'img/world_map/cloud1.png',
-    'img/world_map/cloud2.png',
-    'img/world_map/house1.png',
-    'img/world_map/house2.png',
-    'img/world_map/hut1.png',
-    'img/world_map/hut2.png',
-    'img/world_map/logo.png',
-    'img/world_map/moon1.png',
-    'img/world_map/moon2.png',
-    'img/world_map/zzz1.png',
-    'img/world_map/zzz2.png',
+    'assets/img/world_map/bg1.png',
+    'assets/img/world_map/bg2.png',
+    'assets/img/world_map/bird1.png',
+    'assets/img/world_map/bird2.png',
+    'assets/img/world_map/cactus1.png',
+    'assets/img/world_map/cactus2.png',
+    'assets/img/world_map/city1.png',
+    'assets/img/world_map/city2.png',
+    'assets/img/world_map/cloud1.png',
+    'assets/img/world_map/cloud2.png',
+    'assets/img/world_map/house1.png',
+    'assets/img/world_map/house2.png',
+    'assets/img/world_map/hut1.png',
+    'assets/img/world_map/hut2.png',
+    'assets/img/world_map/logo.png',
+    'assets/img/world_map/moon1.png',
+    'assets/img/world_map/moon2.png',
+    'assets/img/world_map/zzz1.png',
+    'assets/img/world_map/zzz2.png',
 ];
 
 export class WorldMap extends Container {
-    private loader: Loader = new Loader();
+    private loader: Loader = Loader.shared;
 
     constructor() {
         super();
@@ -37,10 +37,8 @@ export class WorldMap extends Container {
      */
     private init = async () => {
         console.log('Start');
-        const didLoad = this.loadAssets().then((res) => {
-            console.log(res);
-        });
-        console.log(didLoad);
+        const didLoad = await this.loadAssets();
+        console.log('did load?', didLoad);
         console.log('LOADER:', this.loader);
     };
 
@@ -52,19 +50,25 @@ export class WorldMap extends Container {
      */
     private loadAssets = async (): Promise<true> => {
         resourceArray.forEach((url) => this.loadAsset(url));
-
         return new Promise((resolve, reject) => {
             this.loader.onComplete.add(() => {
                 console.log('End of Load');
                 resolve(true);
             });
             this.loader.onError.add((err) => {
-                console.error(err);
+                console.error('Loader error:', err);
                 reject(false);
             });
         });
     };
 
+    /**
+     * @method loadAsset
+     * @private
+     * @description Queues a single resource to be loaded by the loader.
+     * @param {string} url The path to the asset from the root of the assets folder (ex: `<root>/assets/img/image.png` becomes `img/image.png`).
+     * @returns {undefined} Promise resolves when all assets are loaded.
+     */
     private loadAsset = (url: string): void => {
         try {
             const name = url.match(/(?<=\/)[^\/]{0,}(?=\.png$)/);
