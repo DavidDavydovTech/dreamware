5051251d61bb01ca94c7b440311a0f454cec2ba7
David J. Davydov <davydovjd611@gmail.com>
2021-02-21 06:46:58 -0500

    Use factory functions

--- src/lib/core/WorldMap.ts
+++ src/lib/core/WorldMap.ts
@@ -1,4 +1,5 @@
-import { Container, Loader } from 'pixi.js';
+import { Container, Sprite } from 'pixi.js';
+import { loadAssets, sharedLoader } from './factory/assetLoader';
 const resourceArray = [
     'assets/img/world_map/bg1.png',
     'assets/img/world_map/bg2.png',
@@ -22,8 +23,6 @@ const resourceArray = [
 ];
 
 export class WorldMap extends Container {
-    private loader: Loader = Loader.shared;
-
     constructor() {
         super();
         this.init();
@@ -36,7 +35,9 @@ export class WorldMap extends Container {
      * @returns {undefined}
      */
     private init = async () => {
-        await this.loadAssets();
+        await this.loadAssets(resourceArray);
+        this.addChild(new Sprite(sharedLoader.resources.bg1.texture));
+        console.log(sharedLoader);
     };
 
     /**
@@ -45,42 +46,7 @@ export class WorldMap extends Container {
      * @description Queues the loader to load all essential textures and audio.
      * @returns {Promise} Promise resolves when all assets are loaded.
      */
-    private loadAssets = async (): Promise<true> => {
-        resourceArray.forEach((url) => this.loadAsset(url));
-        return new Promise((resolve, reject) => {
-            this.loader.onComplete.add(() => {
-                console.log('End of Load');
-                resolve(true);
-            });
-            this.loader.onError.add((err) => {
-                console.error('Loader error:', err);
-                reject(false);
-            });
-        });
-    };
-
-    /**
-     * @method loadAsset
-     * @private
-     * @description Queues a single resource to be loaded by the loader.
-     * @param {string} url The path to the asset from the root of the assets folder (ex: `<root>/assets/img/image.png` becomes `img/image.png`).
-     * @returns {undefined} Promise resolves when all assets are loaded.
-     */
-    private loadAsset = (url: string): void => {
-        try {
-            const name = url.match(/(?<=\/)[^\/]{0,}(?=\.png$)/);
-            if (name === null) {
-                throw new Error(`Tried to get a resource at "${url}" but loadAssets could not decode the file name.`);
-            } else if (name.length > 1) {
-                throw new Error(
-                    `Tried to get a resource at "${url}" but loadAssets parsed two file names instead of one.`
-                );
-            }
-            this.loader.add(name[0], url);
-        } catch (err) {
-            throw err;
-        }
-    };
+    private loadAssets = loadAssets;
 }
 
 export default WorldMap;
