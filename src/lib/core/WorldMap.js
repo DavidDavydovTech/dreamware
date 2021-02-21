9b2c8781e0ffb013f51b368cb866820b6154edcf
David J. Davydov <davydovjd611@gmail.com>
2021-02-21 01:53:52 -0500

    Add WorldMap base file

---
+++ src/lib/core/WorldMap.js
@@ -0,0 +1,10 @@
+import { sup } from "global-prefix";
+import { Container } from "pixi.js";
+
+export class WorldMap extends Container {
+    constructor() {
+        super();
+    }
+}
+
+export default WorldMap;
