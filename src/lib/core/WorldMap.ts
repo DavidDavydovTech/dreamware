81d8db5e5ae938a57ef272c9e70d1094f8d1f130
David J. Davydov <davydovjd611@gmail.com>
2021-02-26 17:04:15 -0500

    Add interactivity

--- src/lib/core/WorldMap.ts
+++ src/lib/core/WorldMap.ts
@@ -25,6 +25,8 @@ const resourceArray = [
 ];
 
 export class WorldMap extends Container {
+    public sprites: { [prop: string]: DoodleSprite } | undefined;
+
     constructor() {
         super();
         this.init();
@@ -45,10 +47,46 @@ export class WorldMap extends Container {
             throw err;
         });
         const textureArray = parseTextures(propertyArray);
-        const spriteCollection = populateSprites(textureArray);
-        for (const sprite in spriteCollection) {
-            this.addChild(spriteCollection[sprite]);
+        this.sprites = populateSprites(textureArray);
+        for (const sprite in this.sprites) {
+            this.addChild(this.sprites[sprite]);
         }
+
+        const { logo, zzz, cloud, moon, hut, city, house, cactus, bird } = this.sprites;
+
+        logo.alpha = 0;
+        zzz.alpha = 0;
+        cloud.alpha = 0;
+        moon.x = 80;
+        moon.y = 25;
+        hut.x = 330 + hut.width / 2;
+        hut.y = 230 + hut.height / 2;
+        city.x = 400 + city.width / 2;
+        city.y = 370 + city.height / 2;
+        house.x = 230;
+        house.y = 630;
+        cactus.x = 80;
+        cactus.y = 610;
+        bird.x = 300;
+        bird.y = 70;
+
+        house.pivot.set(house.width / 2, house.height / 2);
+        house.buttonMode = true;
+        house.interactive = true;
+        house.on('pointerover', () => house.scale.set(1.1, 1.1));
+        house.on('pointerout', () => house.scale.set(1, 1));
+
+        city.pivot.set(city.width / 2, city.height / 2);
+        city.buttonMode = true;
+        city.interactive = true;
+        city.on('pointerover', () => city.scale.set(1.1, 1.1));
+        city.on('pointerout', () => city.scale.set(1, 1));
+
+        hut.pivot.set(hut.width / 2, hut.height / 2);
+        hut.buttonMode = true;
+        hut.interactive = true;
+        hut.on('pointerover', () => hut.scale.set(1.1, 1.1));
+        hut.on('pointerout', () => hut.scale.set(1, 1));
     };
 
     /** @type {import('./factory/assetLoader').loadAssets} */
