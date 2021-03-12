0035c426f4729891f22d090e857dae4ecc5f2f3f
David J. Davydov <davydovjd611@gmail.com>
2021-03-12 15:05:06 -0500

    Build out some of the constructor

--- src/lib/core/MiniGameInstance.ts
+++ src/lib/core/MiniGameInstance.ts
@@ -1,7 +1,20 @@
 import { Container } from 'pixi.js';
 
 export class MiniGameInstance extends Container {
-  constructor() {
+  public init;
+  public update;
+  private _result;
+  public result: Promise<boolean>;
+  public resultResolver: (didWin: boolean) => void;
+  public resultRejector: (error: Error) => void;
+
+  constructor(init: () => void, update: () => void) {
     super();
+    this.init = init;
+    this.update = update;
+    this.result = new Promise((resolve, reject) => {
+      this.resultResolver = resolve;
+      this.resultRejector = reject;
+    });
   }
 }
