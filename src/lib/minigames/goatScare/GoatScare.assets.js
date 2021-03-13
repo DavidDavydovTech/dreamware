c60a3b9950e47c5068b29c9a720644fdfff88c73
David J. Davydov <davydovjd611@gmail.com>
2021-03-12 20:07:31 -0500

    Create assets file for GoatScare

---
+++ src/lib/minigames/goatScare/GoatScare.assets.js
@@ -0,0 +1,20 @@
+const assets = {
+  goatBgA: 'img/miro/goatscare/GOAT_BG_A.png',
+  goatBgB: 'img/miro/goatscare/GOAT_BG_B.png',
+  goatBushA: 'img/miro/goatscare/GOAT_BUSH_A.png',
+  goatBushB: 'img/miro/goatscare/GOAT_BUSH_B.png',
+  goatCalmA: 'img/miro/goatscare/GOAT_CALM_A.png',
+  goatCalmB: 'img/miro/goatscare/GOAT_CALM_B.png',
+  goatGroundA: 'img/miro/goatscare/GOAT_GROUND_A.png',
+  goatGroundB: 'img/miro/goatscare/GOAT_GROUND_B.png',
+  goatScaredA: 'img/miro/goatscare/GOAT_SCARED_A.png',
+  goatScaredB: 'img/miro/goatscare/GOAT_SCARED_B.png',
+  goatSneakA: 'img/miro/goatscare/GOAT_SNEAK_A.png',
+  goatSneakB: 'img/miro/goatscare/GOAT_SNEAK_B.png',
+  goatSpookA: 'img/miro/goatscare/GOAT_SPOOK_A.png',
+  goatSpookB: 'img/miro/goatscare/GOAT_SPOOK_B.png',
+  goatSpinA: 'img/miro/goatscare/SPIN_A.png',
+  goatSpinB: 'img/miro/goatscare/SPIN_B.png',
+};
+
+export default assets;
