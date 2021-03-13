0fa1e635fd84a58d0ca10602e45c853b772785ee
David J. Davydov <davydovjd611@gmail.com>
2021-03-12 20:04:53 -0500

    Add loadGameAssets

--- src/lib/core/factory/assetLoader.ts
+++ src/lib/core/factory/assetLoader.ts
@@ -13,17 +13,17 @@ export const sharedLoader = Loader.shared;
  * @returns {Promise} Promise resolves when all assets are loaded.
  */
 export const loadAssets = async (resourceArray: string[]): Promise<string[]> => {
-    const resourceProperties: string[] = resourceArray.map((url) => loadAsset(url));
-    return new Promise((resolve, reject) => {
-        sharedLoader.onComplete.add(() => {
-            console.log('End of Load');
-            resolve(resourceProperties);
-        });
-        sharedLoader.onError.add((err) => {
-            console.error('Loader error:', err);
-            reject(err);
-        });
+  const resourceProperties: string[] = resourceArray.map((url) => loadAsset(url));
+  return new Promise((resolve, reject) => {
+    sharedLoader.onComplete.add(() => {
+      console.log('End of Load');
+      resolve(resourceProperties);
     });
+    sharedLoader.onError.add((err) => {
+      console.error('Loader error:', err);
+      reject(err);
+    });
+  });
 };
 
 /**
@@ -34,25 +34,25 @@ export const loadAssets = async (resourceArray: string[]): Promise<string[]> => 
  * @returns {undefined} Promise resolves when all assets are loaded.
  */
 export const loadAsset = (url: string): string => {
-    try {
-        const name = url.match(/(?<=\/)[^\/]{0,}(?=\.png$)/);
-        if (name === null) {
-            throw new Error(`Tried to get a resource at "${url}" but loadAssets could not decode the file name.`);
-        } else if (name.length > 1) {
-            throw new Error(`Tried to get a resource at "${url}" but loadAssets parsed two file names instead of one.`);
-        }
-        const resourceName = name[0];
-        if (sharedLoader.resources.hasOwnProperty(resourceName) === false) {
-            sharedLoader.add(resourceName, url);
-        }
-        return resourceName;
-    } catch (err) {
-        throw err;
+  try {
+    const name = url.match(/(?<=\/)[^\/]{0,}(?=\.png$)/);
+    if (name === null) {
+      throw new Error(`Tried to get a resource at "${url}" but loadAssets could not decode the file name.`);
+    } else if (name.length > 1) {
+      throw new Error(`Tried to get a resource at "${url}" but loadAssets parsed two file names instead of one.`);
     }
+    const resourceName = name[0];
+    if (sharedLoader.resources.hasOwnProperty(resourceName) === false) {
+      sharedLoader.add(resourceName, url);
+    }
+    return resourceName;
+  } catch (err) {
+    throw err;
+  }
 };
 
 interface TextureCollection {
-    [prop: string]: Texture;
+  [prop: string]: Texture;
 }
 
 /**
@@ -61,15 +61,15 @@ interface TextureCollection {
  * @param {string[]} resourceArray A array of resource property names.
  */
 export const parseTextures = (resourceArray: string[]): TextureCollection => {
-    const textureObj: TextureCollection = {};
-    for (const texture of resourceArray) {
-        if (sharedLoader.resources.hasOwnProperty(texture)) {
-            textureObj[texture] = sharedLoader.resources[texture].texture;
-        } else {
-            throw new Error(`Tried to get ${texture} but it did not exist on the given loader's resources.`);
-        }
+  const textureObj: TextureCollection = {};
+  for (const texture of resourceArray) {
+    if (sharedLoader.resources.hasOwnProperty(texture)) {
+      textureObj[texture] = sharedLoader.resources[texture].texture;
+    } else {
+      throw new Error(`Tried to get ${texture} but it did not exist on the given loader's resources.`);
     }
-    return textureObj;
+  }
+  return textureObj;
 };
 
 // interface SpriteTextureGroups {
@@ -77,12 +77,12 @@ export const parseTextures = (resourceArray: string[]): TextureCollection => {
 // }
 
 interface PopulateSpritesOptions {
-    defaultOptions?: DoodleSpriteOptions;
-    regex?: RegExp;
+  defaultOptions?: DoodleSpriteOptions;
+  regex?: RegExp;
 }
 
 interface DoodleSpriteCollection {
-    [prop: string]: DoodleSprite;
+  [prop: string]: DoodleSprite;
 }
 
 /**
@@ -93,34 +93,51 @@ interface DoodleSpriteCollection {
  * @param {RegExp} regex The regular expression used to decide how to group up textures for DoodleSprite. Your regular expresion should only return one result.
  */
 export const populateSprites = (
-    textureCollection: TextureCollection,
-    { defaultOptions, regex = /[^0-9]{0,}(?=[0-9]{0,})/ }: PopulateSpritesOptions = {}
+  textureCollection: TextureCollection,
+  { defaultOptions, regex = /[^0-9]{0,}(?=[0-9]{0,})/ }: PopulateSpritesOptions = {}
 ): DoodleSpriteCollection => {
-    const groups: { [prop: string]: Texture[] } = {};
-    const result: DoodleSpriteCollection = {};
+  const groups: { [prop: string]: Texture[] } = {};
+  const result: DoodleSpriteCollection = {};
 
-    for (const textureName in textureCollection) {
-        const groupName = textureName.match(regex);
-        if (groupName === null) {
-            throw new Error(
-                `Tried to get a group name from "${textureName}" but populateSprites could not convert the texture name in to a group name.`
-            );
-        } else if (groupName.length > 1) {
-            throw new Error(
-                `Tried to get a group name from "${textureName}" but populateSprites parsed two or more group names instead of one. (double check your regular expression: "${regex}")`
-            );
-        }
-        console.log(groupName[0]);
-        if (groups.hasOwnProperty(groupName[0]) === false) {
-            groups[groupName[0]] = [];
-        }
-
-        groups[groupName[0]].push(textureCollection[textureName]);
+  for (const textureName in textureCollection) {
+    const groupName = textureName.match(regex);
+    if (groupName === null) {
+      throw new Error(
+        `Tried to get a group name from "${textureName}" but populateSprites could not convert the texture name in to a group name.`
+      );
+    } else if (groupName.length > 1) {
+      throw new Error(
+        `Tried to get a group name from "${textureName}" but populateSprites parsed two or more group names instead of one. (double check your regular expression: "${regex}")`
+      );
+    }
+    console.log(groupName[0]);
+    if (groups.hasOwnProperty(groupName[0]) === false) {
+      groups[groupName[0]] = [];
     }
 
-    for (const group in groups) {
-        result[group] = new DoodleSprite(groups[group], defaultOptions);
-    }
+    groups[groupName[0]].push(textureCollection[textureName]);
+  }
 
-    return result;
+  for (const group in groups) {
+    result[group] = new DoodleSprite(groups[group], defaultOptions);
+  }
+
+  return result;
 };
+
+export async function loadGameAssets(assetsObject: { [prop: string]: string }, callback?: () => void): Promise<void> {
+  return new Promise((res, rej) => {
+    sharedLoader.add(assetsObject);
+
+    sharedLoader.onComplete.once(() => {
+      callback && callback();
+      res();
+    });
+
+    sharedLoader.onError.once(() => {
+      rej();
+    });
+
+    sharedLoader.load();
+  });
+}
