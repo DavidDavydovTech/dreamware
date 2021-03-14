1dc7e58ac758aac423a6bcb300e01ce7f24fb4a7
David J. Davydov <davydovjd611@gmail.com>
2021-03-14 17:59:44 -0400

    Make a assets file for NightPlug

---
+++ src/lib/minigames/nightPlug/NightPlug.assets.ts
@@ -0,0 +1,33 @@
+export const assetsObject = {
+  plugArmDarkA: 'img/impy/nightplug/arm_0000.png',
+  plugArmDarkB: 'img/impy/nightplug/arm_0001.png',
+  plugArmLightA: 'img/impy/nightplug/armlight_0000.png',
+  plugArmLightB: 'img/impy/nightplug/armlight_0001.png',
+  plugArmDarkPluggedA: 'img/impy/nightplug/armplugged_0000.png',
+  plugArmDarkPluggedB: 'img/impy/nightplug/armplugged_0001.png',
+  plugArmLightPluggedA: 'img/impy/nightplug/armlightplugged_0000.png',
+  plugArmLightPluggedB: 'img/impy/nightplug/armlightplugged_0001.png',
+  plugSocketA: 'img/impy/nightplug/socket_0000.png',
+  plugSocketB: 'img/impy/nightplug/socket_0001.png',
+  plugSocketLightA: 'img/impy/nightplug/socketlight_0000.png',
+  plugSocketLightB: 'img/impy/nightplug/socketlight_0001.png',
+  plugBgA: 'img/impy/nightplug/bg_0000.png',
+  plugBgB: 'img/impy/nightplug/bg_0001.png',
+  plugDarknessA: 'img/impy/nightplug/darkness_0000.png',
+};
+
+export const texturesArray = [
+  'plugArmDarkA',
+  'plugArmDarkB',
+  'plugArmLightPluggedA',
+  'plugArmLightPluggedB',
+  'plugSocketLightA',
+  'plugSocketLightB',
+  'plugBgA',
+  'plugBgB',
+  'plugDarknessA',
+  'plugSocketA',
+  'plugSocketB',
+];
+
+export default assetsObject;
